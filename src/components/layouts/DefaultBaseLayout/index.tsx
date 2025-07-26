b0258788aa2b27eb4c3682b87f621058f0091f24
ker0de <150711382+ker0de@users.noreply.github.com>
2025-07-27 04:46:35 +1000

    Update index.tsx

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 import Header from '../../sections/Header';
 import Footer from '../../sections/Footer';
+import Head from 'next/head';
 
 export default function DefaultBaseLayout(props) {
     const { page, site } = props;
@@ -9,12 +10,17 @@ export default function DefaultBaseLayout(props) {
     const pageMeta = page?.__metadata || {};
 
     return (
-        <div className={classNames('sb-page', pageMeta.pageCssClasses)} {...(enableAnnotations && { 'data-sb-object-id': pageMeta.id })}>
-            <div className="sb-base sb-default-base-layout">
-                {site.header && <Header {...site.header} enableAnnotations={enableAnnotations} />}
-                {props.children}
-                {site.footer && <Footer {...site.footer} enableAnnotations={enableAnnotations} />}
+        <>
+            <Head>
+                <link rel="icon" href="/images/logo_favicon.png" />
+            </Head>
+            <div className={classNames('sb-page', pageMeta.pageCssClasses)} {...(enableAnnotations && { 'data-sb-object-id': pageMeta.id })}>
+                <div className="sb-base sb-default-base-layout">
+                    {site.header && <Header {...site.header} enableAnnotations={enableAnnotations} />}
+                    {props.children}
+                    {site.footer && <Footer {...site.footer} enableAnnotations={enableAnnotations} />}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
